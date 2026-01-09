b3f225d0606143a5c5a9f693aaaa6b7b88aaae2d
Edd Mann <the@eddmann.com>
2026-01-09 17:05:58 +0000

    fix(santa-lang-workbench): restore arch intel value

--- Casks/santa-lang-workbench.rb
+++ Casks/santa-lang-workbench.rb
@@ -1,5 +1,5 @@
 cask "santa-lang-workbench" do
-  arch arm: "arm64", intel: "c36a8b5470c7e6dc522d43ebfe61bfc647fc084c25b7f955726e6fd57720f6e9"
+  arch arm: "arm64", intel: "amd64"
 
   version "0.1.0"
   sha256 arm:   "54a20c9016fa89b79431f619fa2472f69b260c9ec83c7523a54e363d2defa663",
